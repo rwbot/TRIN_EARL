abcf4db9b0420bcda704f09a9bded86c2bbc1f2c
Jin Pyo Jeon <jinpyo.jeon@trincoll.edu>
2017-01-29 19:27:25 -0500

    Updated motor test file

--- igvc/src/test/motor_test.py
+++ igvc/src/test/motor_test.py
@@ -3,6 +3,14 @@ import serial
 
 # configure the serial connections (the parameters differs on the device you are connecting to
 
+ser = serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate=9600,
+    timeout=1,
+    parity=serial.PARITY_EVEN,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.SEVENBITS,
+)
 
 def get_response():
     resp = ''
@@ -10,23 +18,35 @@ def get_response():
         resp += ser.read(1)
     return resp
 
+def init_serial_mode():
+    for i in range(0, 10):
+        ser.write('\r')
+    
+    resp = get_response() 
+    if (resp != 'OK'):
+        print (resp)
+        print ("Did not get OK message")
+    
+def write_byte(string):
+    ser.write((string + '\r').encode())
 
 def main():
+    #if not ser.is_open:
+    #    print ("Port is not accessible")
+    #    eixt()
+    
+    ser.setRTS(False)
+    # ser.setDTR(False)
 
-    ser = serial.Serial(
-        port='/dev/ttyUSB1',
-        baudrate=9600,
-        parity=serial.PARITY_EVEN,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.SEVENBITS
-    )
+    print(ser.getCTS())
+    print(ser.getDSR())
 
     #  10 Enter key press (new line) is needed for RS-232 Mode
-    ser.write("\n\n\n\n\n\n\n\n\n\n")
-    if (get_response() != 'OK'):
-        print ("Did not get OK message")
-        ser.close()
-        exit()
+    # for i in range(0, 10):
+    #     ser.write("\r")
+    
+   #    ser.close()
+    #    exit()
 
     while True:
         prompt = """
@@ -35,6 +55,7 @@ def main():
         command = test both wheels
         query = test if data can be retrieved from serial port
         exit = close port and terminate program
+        init = start rs232 mode for motor controller
         """
         print(prompt)
         
@@ -45,32 +66,36 @@ def main():
             exit()
         
         elif input == 'command':
-            ser.write("!A10")
+            write_byte("!A10")
             print(get_response())
             time.sleep(1)
 
-            ser.write("!a10")
+            write_byte("!a10")
             print(get_response())
             time.sleep(1)
 
-            ser.write("!A00")
+            write_byte("!A00")
 
-            ser.write("!B10")
+            write_byte("!B10")
             print(get_response())
             time.sleep(1)
 
-            ser.write("!b10")
+            write_byte("!b10")
             print(get_response())
             time.sleep(1)
 
-            ser.write("!B00")
+            write_byte("!B00")
+            print(get_response())
+            time.sleep(1)
 
         elif input == 'query':
-            ser.write('?K')
+            write_byte('?K')
             print ("Speed query %s" % get_response())
 
-            ser.write('?E')
+            write_byte('?E')
             print ("Battery query %s" % get_response())
 
+        elif input == 'init':
+            init_serial_mode()
 if __name__ == '__main__':
     main()
